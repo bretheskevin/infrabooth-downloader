f98cc828c5a983176cc43cd3e3607fdb28b62927
Kévin B <k.brethes@myunisoft.fr>
2026-03-05 10:00:25 +0100

    feat: update frontend for browser cookie authentication

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,6 @@
 export { useLanguageSync } from '@/features/settings/hooks/useLanguageSync';
 export { useThemeSync } from '@/features/settings/hooks/useThemeSync';
-export { useAuthCallback } from '@/features/auth/hooks/useAuthCallback';
 export { useAuthStateListener } from '@/features/auth/hooks/useAuthStateListener';
-export { useOAuthFlow } from '@/features/auth/hooks/useOAuthFlow';
 export { useStartupAuth } from '@/features/auth/hooks/useStartupAuth';
 export { useDebounce } from './useDebounce';
 export { useInitializeSettings } from '@/features/settings/hooks/useInitializeSettings';
